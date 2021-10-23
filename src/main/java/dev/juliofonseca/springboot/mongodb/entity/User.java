30e98004ff7ca81b63019d7c5ad9ba8566ed81c5
Julio Fonseca <juliocsrmf@gmail.com>
2021-10-23 20:31:21 -0300

    User entity and resource

---
+++ src/main/java/dev/juliofonseca/springboot/mongodb/entity/User.java
@@ -0,0 +1,42 @@
+package dev.juliofonseca.springboot.mongodb.entity;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class User implements Serializable {
+
+    private String id;
+    private String name;
+    private String email;
+
+    public User(String id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
