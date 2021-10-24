8a808a896a1aaa0dedfaacb715f6e9f1f8e58900
Julio Fonseca <juliocsrmf@gmail.com>
2021-10-24 02:37:42 -0300

    @Query with multiple fields and conditions

--- src/main/java/dev/juliofonseca/springboot/mongodb/service/PostService.java
+++ src/main/java/dev/juliofonseca/springboot/mongodb/service/PostService.java
@@ -2,11 +2,13 @@ package dev.juliofonseca.springboot.mongodb.service;
 
 import dev.juliofonseca.springboot.mongodb.entity.Post;
 
+import java.util.Date;
 import java.util.List;
 
 public interface PostService {
 
     Post findById(String id);
     List<Post> findByTitle(String text);
+    List<Post> fullSearch(String text, Date minDate, Date maxDate);
 
 }
--- src/main/java/dev/juliofonseca/springboot/mongodb/util/URL.java
+++ src/main/java/dev/juliofonseca/springboot/mongodb/util/URL.java
@@ -2,9 +2,24 @@ package dev.juliofonseca.springboot.mongodb.util;
 
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 public class URL {
     public static String decodeParam(String text) {
         return URLDecoder.decode(text, StandardCharsets.UTF_8);
+    }
+
+    public static Date converDate(String date, Date defaultValue) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+
+        try {
+            return sdf.parse(date);
+        } catch (ParseException e) {
+            return defaultValue;
+        }
     }
 }
